f7dcdefcc1a04d100e3cabda2b4d41b2f49be0fd
toStrin <1050699053@qq.com>
2020-09-24 23:24:13 +0800

    first commit

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -2,5 +2,6 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("11");
         System.out.println("2222");
+        System.out.println("222");
     }
 }
